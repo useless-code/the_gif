db8f013fbbba13d7aa8fe4b57be1a3fd479dc6ae
Joaquín Sorianello <joac@joac.com.ar>
2013-08-27 23:35:36 -0300

    multiple albums

--- app.picassa.js
+++ app.picassa.js
@@ -3,36 +3,60 @@ $(document).ready(function (){
   function build_data_url(user, album) {
     return "https://picasaweb.google.com/data/feed/api/user/" + user + "/albumid/"+ album +"?alt=json&kind=photo"
   };
-  var user = "113288886837539699792",
-    album = "5898807933959509569",
-    gif_list = [];
-    
-  $.get(build_data_url(user, album), function(response) {
+  var gif_list = [],
+    copy_of_gift_list = [];
+  var albums = [
+    {id: "5898807933959509569", user: "113288886837539699792",},
+    {id: "5911386033286256529", user: "114337695162527210679",},
+  ];
+
+  function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  
+
+  function process_response(response) {
     var data_list = response.feed.entry;
     for(var i=0; i<data_list.length; i++){
-        gif_list.push(data_list[i].content.src);
+      var url = data_list[i].content.src;
+      gif_list.push(url);
+      copy_of_gift_list.push(url)
     };
-
-    var copy_of_gift_list = gif_list.slice(0);
-
-    function getRandomInt (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+  };
     
-    function random_element(){
-        if (copy_of_gift_list.length == 0) {
-            copy_of_gift_list = gif_list.slice(0);
-        };
-        var index = getRandomInt(0, copy_of_gift_list.length - 1);
-        return copy_of_gift_list.splice(index, 1);
+  function random_gif(){
+    if (copy_of_gift_list.length == 0) {
+      copy_of_gift_list = gif_list.slice(0);
     };
-
+    var index = getRandomInt(0, copy_of_gift_list.length - 1);
+      return copy_of_gift_list.splice(index, 1);
+  };
+  
     var buffer = new window.Buffer({
-        data_source: random_element, 
+        data_source: random_gif, 
         limit: 10,
-    });
+  });
 
+  function get_random_album() {
+    var index = getRandomInt(0, albums.length - 1);
+    var album = albums.splice(index, 1)[0];
+    return build_data_url(album.user, album.id)
+  };
+
+  function process_next(response){
+    if (albums.length > 0) {
+      $.get(get_random_album(), process_next)
+    }
+    process_response(response);
+  }
+
+
+  $.get(get_random_album(), function(response) {
+    
+    process_next(response);
     buffer.start();
+  
     $load_bar = $("#fadingBarsG");
     $(document).click(function() {
         if (buffer.waiting()){
