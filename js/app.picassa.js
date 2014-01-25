3d9446eb0542dcd9bd1494d2548c346c5c018ed3
Joaquín Sorianello <joac@joac.com.ar>
2014-01-24 22:23:37 -0300

    Refactor

--- js/app.picassa.js
+++ js/app.picassa.js
@@ -1,91 +1,112 @@
 //Wrapped in a namespace, because we are good javascript coders
 $(document).ready(function () {
   "use strict";
-  function build_data_url(user, album) {
-    return "https://picasaweb.google.com/data/feed/api/user/" +
-          user + "/albumid/" +
-          album + "?alt=json&kind=photo";
-  }
-  var gif_list = [],
-    copy_of_gift_list = [];
-  var albums = [
-    {id: "5898807933959509569", user: "113288886837539699792",},
-    {id: "5911386033286256529", user: "114337695162527210679",},
-    {id: "5932806704917310497", user: "114337695162527210679",},
-  ];
+  var theGif, gif_list = [],
+    CACHE_MAX = 10,
+    copy_of_gift_list = [],
+    albums = [
+        {id: "5898807933959509569", user: "113288886837539699792",},
+        {id: "5911386033286256529", user: "114337695162527210679",},
+        {id: "5932806704917310497", user: "114337695162527210679",},
+    ];
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+    theGif = {
+        favicon: document.getElementById('favicon'),
+        gifList: [],
+        gifListCopy: [],
+        initialGif: null,
+        buildDataUrl: function (user, album) {
+            return "https://picasaweb.google.com/data/feed/api/user/" +
+                user + "/albumid/" +
+                album + "?alt=json&kind=photo";
+        },
+        getRandomInt: function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        processFeedResponse: function (response) {
+            var url,
+                dataList = response.feed.entry,
+                i,
+                l = dataList.length;
 
+            for (i = 0; i < l; i++) {
+                var url = dataList[i].content.src;
+                this.gifList.push(url);
+                this.gifListCopy.push(url);
+            }
+        },
+        buildGifMetadata: function(index, feed) {
 
+        },
 
-  function process_response(response) {
-    var data_list = response.feed.entry;
-    for (var i = 0; i < data_list.length; i++) {
-      var url = data_list[i].content.src;
-      gif_list.push(url);
-      copy_of_gift_list.push(url);
-    }
-  }
+        run: function () {
+            this.buffer = new window.Buffer({
+                data_source: this.randomGif.bind(this),
+                limit: CACHE_MAX,
+            }),
+            window.start_loading();
+            $(document).click(
+                this.onClick.bind(this)
+            );
+            this.getAllAlbums().done(function () {
+                this.buffer.start();
+                if(!this.initialGif) {
+                    $(document).click();
+                }
+            }.bind(this));
+        },
+        onClick: function () {
+            if (this.buffer.waiting()) {
+                return;
+            }
+            window.start_loading();
+            this.buffer.get(this.updateGif.bind(this));
+        },
+        updateGif: function (image) {
+            var w = image.width, h = image.height;
 
-  function random_gif() {
-    var index;
+            if (h < 270 && w > h) {
+                var size = Math.floor((270 / h) * w);
+                document.body.style.backgroundSize = size + 'px';
+            } else if (w < 300) {
+                document.body.style.backgroundSize = '300px';
+            } else {
+                document.body.style.backgroundSize = 'auto';
+            }
+            document.body.style.backgroundImage = 'url("' + image.src  + '")';
+            this.updateFavicon(image);
+            window.end_loading();
+        },
+        updateFavicon: function (image) {
+            document.head.removeChild(this.favicon);
+            this.favicon.href = image.src;
+            document.head.appendChild(this.favicon);
+        },
 
-    if (copy_of_gift_list.length === 0) {
-      copy_of_gift_list = gif_list.slice(0);
-    }
-    index = getRandomInt(0, copy_of_gift_list.length - 1);
+        getRandomAlbumUrl: function () {
+            var index = this.getRandomInt(0, albums.length - 1),
+                album = albums.splice(index, 1)[0];
+            return this.buildDataUrl(album.user, album.id);
+        },
+        randomGif: function () {
+            var index;
+            if (this.gifListCopy.length === 0) {
+                this.gifListCopy = this.gifList.slice(0);
+            }
+            index = this.getRandomInt(0, this.gifListCopy.length - 1);
+            return this.gifListCopy.splice(index, 1);
+        },
 
-    return copy_of_gift_list.splice(index, 1);
-  }
-
-  var buffer = new window.Buffer({
-    data_source: random_gif,
-    limit: 10,
-  });
-
-  function get_random_album() {
-    var index = getRandomInt(0, albums.length - 1);
-    var album = albums.splice(index, 1)[0];
-    return build_data_url(album.user, album.id);
-  }
-
-  function process_next(response) {
-    if (albums.length > 0) {
-      $.get(get_random_album(), process_next);
-    }
-    process_response(response);
-  }
-
-
-  $.get(get_random_album(), function (response) {
-
-    process_next(response);
-    buffer.start();
-
-    var favicon = document.getElementById('favicon');
-    $(document).click(function () {
-      if (buffer.waiting()) {
-        return;
-      }
-      window.start_loading();
-      buffer.get(function (image) {
-        if (image.height < 270 && image.width > image.height) {
-          var size = Math.floor((270 / image.height) * image.width);
-          document.body.style.backgroundSize = size + 'px';
-        } else if (image.width < 300) {
-          document.body.style.backgroundSize = '300px';
-        } else {
-          document.body.style.backgroundSize = 'auto';
-        }
-        document.body.style.backgroundImage = 'url("' + image.src  + '")';
-        document.head.removeChild(favicon);
-        favicon.href = image.src;
-        document.head.appendChild(favicon);
-        window.end_loading();
-      });
-    });
-    $(document).click();
-  });
+        getAllAlbums: function() {
+            return $.get(this.getRandomAlbumUrl()).done(
+                function (data) {
+                    if (albums.length > 0) {
+                        this.getAllAlbums();
+                    }
+                    this.processFeedResponse(data);
+                }.bind(this));
+        },
+    };
+    theGif.run();
+    window.theGif = theGif;
 });
